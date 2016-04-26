3640b4a260fc1988e5f1203f04c1e99d7e4412a4
Denis Nabokov <zastiralkin@gmail.com>
2016-04-26 23:44:15 +0300

    some minor changes

--- app/src/main/java/ru/chocco_crokko/musikant/activities/MainActivity.java
+++ app/src/main/java/ru/chocco_crokko/musikant/activities/MainActivity.java
@@ -14,7 +14,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -128,7 +127,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("qwe", "resume");
         if (adapter != null)
             changeSortingInAdapter(currentSort, currentSearch, false);
     }
@@ -320,6 +318,7 @@ public class MainActivity extends AppCompatActivity
             public void run() {
                 swipeRefreshLayout.setRefreshing(false);
                 fillDatabaseIfIsNotEmpty();
+                changeSortingInAdapter(currentSort, currentSearch, false);
             }
         }, 500);
     }
