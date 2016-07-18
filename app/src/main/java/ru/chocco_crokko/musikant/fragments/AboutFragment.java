569e4ac0befd8b0fa37eb37e224501963fb0277f
Denis Nabokov <zastiralkin@gmail.com>
2016-07-18 14:28:41 +0300

    changes in About

--- app/src/main/java/ru/chocco_crokko/musikant/fragments/AboutFragment.java
+++ app/src/main/java/ru/chocco_crokko/musikant/fragments/AboutFragment.java
@@ -1,21 +1,25 @@
 package ru.chocco_crokko.musikant.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import ru.chocco_crokko.musikant.R;
 
 
 public class AboutFragment extends DialogFragment {
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        getDialog().setTitle(R.string.about);
-        View v = inflater.inflate(R.layout.about, null);
-        return v;
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.about, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.about).setPositiveButton("OK", null).setView(view);
+        return builder.create();
     }
 }
